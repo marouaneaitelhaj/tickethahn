426c50086648947196dcc7524f51fc7ad56244ee
marwanee <marwaneaitelhaj001@gmail.com>
2025-02-22 18:57:20 +0100

    first commit

---
+++ src/main/java/com/wi/tickethahn/TickethahnApplication.java
@@ -0,0 +1,13 @@
+package com.wi.tickethahn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickethahnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickethahnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wi/tickethahn/TickethahnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wi.tickethahn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickethahnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
