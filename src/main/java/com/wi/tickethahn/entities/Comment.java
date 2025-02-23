27755c199d953904093e2226a4bbbae7ab095921
MAROUANE AIT ELHAJ <marouaneaitelhaj001@gmail.com>
2025-02-23 17:56:24 +0000

    Refactor Docker and application configuration; update docker-compose for network settings, migrate properties to YAML format, and enhance Comment entity with UUID generation

--- src/main/java/com/wi/tickethahn/entities/Comment.java
+++ src/main/java/com/wi/tickethahn/entities/Comment.java
@@ -2,6 +2,8 @@ package com.wi.tickethahn.entities;
 
 import java.util.UUID;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -16,6 +18,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class Comment {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @ManyToOne
